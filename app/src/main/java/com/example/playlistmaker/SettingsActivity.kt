ad80f5aa9d459724487f44e4fec84a88ec767c10
knado <kos891@gmail.com>
2024-09-17 22:03:44 +0300

    back button listener

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        findViewById<Toolbar>(R.id.settings_toolbar).setNavigationOnClickListener {
+            finish()
+        }
     }
 }
