a379d4691d4710affab00e6f28e8c5e0e7d1f18a
knado <kos891@gmail.com>
2024-11-12 21:24:03 +0300

    sprint 11 finished

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,71 +1,60 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.appcompat.widget.Toolbar
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.adapters.SearchViewAdapter
+import com.example.playlistmaker.api.RetrofitHelper.retrofit
+import com.example.playlistmaker.api.SearchAPI
+import com.example.playlistmaker.api.SearchResponse
+import com.example.playlistmaker.helpers.PlaceHolder
+import com.example.playlistmaker.helpers.PlaceHolder.ERROR
+import com.example.playlistmaker.helpers.PlaceHolder.NOT_FOUND
+import com.example.playlistmaker.helpers.PlaceHolder.SEARCH_RESULT
 import com.example.playlistmaker.model.Track
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
 
     private lateinit var clearSearchButton: ImageView
     private lateinit var searchInput: EditText
     private var searchQuery = ""
+    private val tracks = ArrayList<Track>()
+    private val serviceSearch = retrofit.create(SearchAPI::class.java)
+    private lateinit var searchResultRv: RecyclerView
+    private lateinit var placeholderNothingWasFound: TextView
+    private lateinit var placeholderCommunicationsProblem: LinearLayout
+    private lateinit var buttonRetry: Button
 
     companion object {
         const val SAVED_SEARCH = "SAVED_SEARCH"
-        private var SEARCH_STUB = arrayListOf(
-            Track(
-                "Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-            ),
-            Track(
-                "Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg",
-            ),
-            Track(
-                "Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg",
-            )
-        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        findViewById<Toolbar>(R.id.search_toolbar).setNavigationOnClickListener() {
+        findViewById<Toolbar>(R.id.search_toolbar).setNavigationOnClickListener {
             finish()
         }
 
-        searchInput = findViewById(R.id.input_search_form)
-        searchInput.requestFocus()
-        searchInput.addTextChangedListener(inputTextWatcher)
+        placeholderNothingWasFound = findViewById(R.id.placeholderNotFound)
+        placeholderCommunicationsProblem = findViewById(R.id.placeholderCommunication)
 
         clearSearchButton = findViewById(R.id.clear_form)
         clearSearchButton.visibility = View.GONE
@@ -73,9 +62,51 @@ class SearchActivity : AppCompatActivity() {
             clearSearchForm()
         }
 
-        val searchResultRv = findViewById<RecyclerView>(R.id.recycler_view)
-        searchResultRv.adapter = SearchViewAdapter(SEARCH_STUB)
+        searchInput = findViewById(R.id.input_search_form)
+        searchInput.requestFocus()
+        searchInput.addTextChangedListener(inputTextWatcher)
+        searchInput.setText(searchQuery)
+        searchInput.requestFocus()
 
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                getTrack()
+                true
+            }
+            false
+        }
+
+        searchResultRv = findViewById(R.id.recycler_view)
+        searchResultRv.adapter = SearchViewAdapter(tracks)
+
+        buttonRetry = findViewById(R.id.retryButton)
+        buttonRetry.setOnClickListener {
+            getTrack()
+        }
+
+    }
+
+    private fun getTrack() {
+        serviceSearch.searchTrack(searchInput.text.toString())
+            .enqueue(object : Callback<SearchResponse> {
+                override fun onResponse(
+                    call: Call<SearchResponse>,
+                    response: Response<SearchResponse>,
+                ) {
+                    if (searchQuery.isNotEmpty() && !response.body()?.results.isNullOrEmpty() && response.code() == 200) {
+                        tracks.clear()
+                        tracks.addAll(response.body()?.results!!)
+                        searchResultRv.adapter?.notifyDataSetChanged()
+                        showPlaceholder(SEARCH_RESULT)
+                    } else {
+                        showPlaceholder(NOT_FOUND)
+                    }
+                }
+
+                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                    showPlaceholder(ERROR)
+                }
+            })
     }
 
     private val inputTextWatcher = object : TextWatcher {
@@ -96,6 +127,16 @@ class SearchActivity : AppCompatActivity() {
             val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             imm.hideSoftInputFromWindow(view.windowToken, 0)
         }
+
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        searchInput.setText("")
+        inputMethodManager?.hideSoftInputFromWindow(searchInput.windowToken, 0)
+
+        placeholderNothingWasFound.isVisible = false
+        placeholderNothingWasFound.isVisible = false
+        tracks.clear()
+        searchResultRv.adapter?.notifyDataSetChanged()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -107,6 +148,27 @@ class SearchActivity : AppCompatActivity() {
         super.onRestoreInstanceState(savedInstanceState)
         searchQuery = savedInstanceState.getString(SAVED_SEARCH, "")
         searchInput.setText(searchQuery)
+    }
+
+    private fun showPlaceholder(placeholder: PlaceHolder) {
+
+        when (placeholder) {
+            NOT_FOUND -> {
+                searchResultRv.visibility = View.GONE
+                placeholderCommunicationsProblem.visibility = View.GONE
+                placeholderNothingWasFound.visibility = View.VISIBLE
+            }
+            ERROR -> {
+                searchResultRv.visibility = View.GONE
+                placeholderNothingWasFound.visibility = View.GONE
+                placeholderCommunicationsProblem.visibility = View.VISIBLE
+            }
+            else -> {
+                searchResultRv.visibility = View.VISIBLE
+                placeholderNothingWasFound.visibility = View.GONE
+                placeholderCommunicationsProblem.visibility = View.GONE
+            }
+        }
     }
 
 }
