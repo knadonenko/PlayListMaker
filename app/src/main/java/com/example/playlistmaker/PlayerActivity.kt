c174503ce798cec3bea86b0ef8e18e555662158b
knado <kos891@gmail.com>
2025-02-20 21:20:30 +0300

    Плеер работает, вроде

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,13 +1,19 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.helpers.AppConstants.RELOAD_PROGRESS
+import com.example.playlistmaker.helpers.AppConstants.TIME_ZERO
 import com.example.playlistmaker.helpers.IntentConstants
 import com.example.playlistmaker.helpers.TimeHelper.convertDate
 import com.example.playlistmaker.helpers.TimeHelper.convertTimeFromMillis
@@ -27,6 +33,27 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var releaseDate: TextView
     private lateinit var primaryGenreName: TextView
     private lateinit var country: TextView
+    private lateinit var trackProgress: TextView
+    private lateinit var playButton: ImageButton
+
+    private val play = R.drawable.play_circle
+    private val pause = R.drawable.pause_circle
+
+    private var mainHandler: Handler? = null
+
+    private var mediaPlayer = MediaPlayer()
+
+    private val runThread = object: Runnable {
+        override fun run() {
+            trackProgress.text =
+                SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+
+            mainHandler?.postDelayed(
+                this,
+                RELOAD_PROGRESS
+            )
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +63,10 @@ class PlayerActivity : AppCompatActivity() {
             finish()
         }
 
-        val track = Gson().fromJson(intent.getStringExtra(IntentConstants.TRACK), Track::class.java)
+        val track = Gson().fromJson(intent.getStringExtra(IntentConstants.TRACK),
+            Track::class.java)
+
+        mainHandler = Handler(Looper.getMainLooper())
 
         trackName = findViewById(R.id.trackName)
         artistName = findViewById(R.id.artistName)
@@ -46,6 +76,8 @@ class PlayerActivity : AppCompatActivity() {
         releaseDate = findViewById(R.id.release_date_data)
         primaryGenreName = findViewById(R.id.primary_genre_name)
         country = findViewById(R.id.country_data)
+        trackProgress = findViewById(R.id.track_progress)
+        playButton = findViewById(R.id.play_button)
 
         Glide
             .with(albumIcon)
@@ -80,6 +112,90 @@ class PlayerActivity : AppCompatActivity() {
             collectionName.visibility = View.GONE
             collectionNameTitle.visibility = View.GONE
         }
+
+        preparePlayer(track)
+
+        playButton.setOnClickListener {
+            playbackControl()
+            progressTimeControl()
+        }
+
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+            mainHandler?.removeCallbacks(runThread)
+            playButton.setBackgroundResource(play)
+            trackProgress.text = TIME_ZERO
+        }
     }
+
+    private fun preparePlayer(track: Track) {
+        mediaPlayer.setDataSource(track.previewUrl)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playButton.isEnabled = true
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setBackgroundResource(play)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun progressTimeControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                mainHandler?.postDelayed(
+                    runThread,
+                    RELOAD_PROGRESS
+                )
+            }
+            STATE_PAUSED -> {
+                mainHandler?.removeCallbacks(runThread)
+            }
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setBackgroundResource(pause)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setBackgroundResource(play)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+        mainHandler?.removeCallbacks(runThread)
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
+    private var playerState = STATE_DEFAULT
 
 }
--- app/src/main/java/com/example/playlistmaker/helpers/AppConstants.kt
+++ app/src/main/java/com/example/playlistmaker/helpers/AppConstants.kt
@@ -4,4 +4,7 @@ object AppConstants {
 
     const val SEARCH_DEBOUNCE_DELAY = 2000L
 
+    const val RELOAD_PROGRESS = 300L
+    const val TIME_ZERO = "00:00"
+
 }
