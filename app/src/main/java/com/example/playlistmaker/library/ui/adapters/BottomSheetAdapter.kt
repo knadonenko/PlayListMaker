8db1f5aa4ed29d42b3a3e5b5ec41eb5dc1b4113d
knadonenko <kos891@gmail.com>
2025-06-15 12:34:13 +0300

    sprint 22 layouts and bottom sheet

---
+++ app/src/main/java/com/example/playlistmaker/library/ui/adapters/BottomSheetAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.playlistmaker.library.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.databinding.BottomSheetItemBinding
+import com.example.playlistmaker.library.data.Playlist
+import com.example.playlistmaker.library.ui.viewholders.BottomSheetViewHolder
+
+class BottomSheetAdapter(private val clickListener: PlaylistClickListener) :
+    RecyclerView.Adapter<BottomSheetViewHolder>() {
+
+    val list = mutableListOf<Playlist>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BottomSheetViewHolder {
+        return BottomSheetViewHolder(
+            BottomSheetItemBinding.inflate(LayoutInflater.from(parent.context),
+                parent, false)
+        )
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: BottomSheetViewHolder, position: Int) {
+        val playlistItem = list[holder.adapterPosition]
+        holder.bind(playlistItem)
+        holder.itemView.setOnClickListener { clickListener.onPlaylistClick(playlistItem) }
+    }
+
+    fun interface PlaylistClickListener {
+        fun onPlaylistClick(playlist: Playlist)
+    }
+
+}
